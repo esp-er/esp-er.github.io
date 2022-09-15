aefd014552e5f860b807d0d0735339547aa5e30a
Patrik Eriksson <patrikersn@outlook.com>
2022-09-15 11:55:51 +0900

    update site

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202209151029",
+        "/css/pixyll.css?202209151155",
         "/"
       ]);
     })
