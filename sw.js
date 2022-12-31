4a2e54e1caa6d14f2a1447e561b9339fa306daf9
Patrik Eriksson <patrikersn@outlook.com>
2023-01-01 02:57:44 +0900

    update screenshots

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202212311957",
+        "/css/pixyll.css?202301010257",
         "/"
       ]);
     })
