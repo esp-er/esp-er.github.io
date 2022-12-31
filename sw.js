5a1afb55dea890c6dd6aa671dad2d4a0993041ec
Patrik Eriksson <patrikersn@outlook.com>
2022-12-31 19:04:48 +0900

    add aoc '22 animation

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202212311903",
+        "/css/pixyll.css?202212311904",
         "/"
       ]);
     })
